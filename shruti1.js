05c62d056f9b0b7e531924c6795263191bf8aaa9
Shruti <shruti170799@gmail.com>
2021-07-30 22:27:46 +0530

    Modified js

--- shruti1.js
+++ shruti1.js
@@ -1,12 +1,12 @@
 // program to check if a number is prime or not
 
 // take input from the user
-const number = parseInt(prompt("Enter a positive number: "));
+const number = parseInt(prompt("Enter a positive number));
 let isPrime = true;
 
 // check if number is equal to 1
 if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
+    console.log("1 is neither prime nor composite number");
 }
 
 // check if number is greater than 1
